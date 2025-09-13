3e58610ed7bf8d1633f3ad1461b02816e68f6249
Abiraham Immanvel <abirahamimmanvel@gmail.com>
2025-09-13 16:43:59 +0000

    New theme added

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { Typography } from "antd";
+import { Button, Typography } from "antd";
+import { IoMdRefresh } from "react-icons/io";
 import UserTable from "../components/UserTable/UserTable";
 import Filter from "../components/Filter/Filter";
 import AppliedFilters from "../components/AppliedFilters/AppliedFilters";
 import { isSameDay } from "date-fns";
+import TitleBar from "../components/TitleBarCommon/TitleBar";
 
 export type UserRole =
   | "Admin"
@@ -396,46 +398,51 @@ const Users = () => {
     return true;
   });
   return (
-    <div className="h-full w-full flex flex-col">
-      <div className="h-full w-full flex justify-center px-2 sm:px-4 lg:px-6 xl:px-4 2xl:px-6">
-        <div className="w-full max-w-6xl xl:max-w-7xl flex flex-col">
-          <div className="w-full">
-            <Typography.Title level={2} className="text-xl sm:text-2xl">
-              User Management
-            </Typography.Title>
-          </div>
+    <TitleBar
+      title="User Management"
+      description="Manage and oversee all user accounts within the system."
+      extraContent={
+        <div>
+          <Button
+            icon={<IoMdRefresh />}
+            loading={false}
+            type="primary"
+            onClick={() => {}}
+          >
+            Refresh
+          </Button>
+        </div>
+      }
+    >
+      <div className="w-full h-full flex flex-col gap-4">
+        <Filter<Filters>
+          fields={filterFields}
+          initialValues={filters}
+          onChange={setFilters}
+        />
 
-          <div className="w-full h-full flex flex-col gap-[6px] my-4 ">
-            <Filter<Filters>
-              fields={filterFields}
-              initialValues={filters}
-              onChange={setFilters}
-            />
+        <AppliedFilters<Filters>
+          filters={filters}
+          setFilters={setFilters}
+          labels={{
+            role: "Role",
+            status: "Status",
+            lastLogin: "Last Login",
+            createdAt: "Created At",
+          }}
+          colors={{
+            role: "blue",
+            status: "green",
+            lastLogin: "purple",
+            createdAt: "orange",
+          }}
+        />
 
-            <AppliedFilters<Filters>
-              filters={filters}
-              setFilters={setFilters}
-              labels={{
-                role: "Role",
-                status: "Status",
-                lastLogin: "Last Login",
-                createdAt: "Created At",
-              }}
-              colors={{
-                role: "blue",
-                status: "green",
-                lastLogin: "purple",
-                createdAt: "orange",
-              }}
-            />
-
-            <div className="flex-grow overflow-hidden">
-              <UserTable data={filteredData} />
-            </div>
-          </div>
+        <div className="flex-grow overflow-auto rounded-lg border border-gray-300">
+          <UserTable data={filteredData} />
         </div>
       </div>
-    </div>
+    </TitleBar>
   );
 };
 
