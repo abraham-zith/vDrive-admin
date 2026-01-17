f5ea40947dd287c86ea7f4e8f729258bf67f4cea
Abiraham Immanvel <abirahamimmanvel@gmail.com>
2026-01-17 08:07:21 +0000

    feat: implement Socket.IO client integration using React Context and Provider.

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { Provider } from "react-redux";
 import App from "./App.tsx";
 import "./index.css";
 import { store } from "./store";
+import { SocketProvider } from "./contexts/SocketProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <SocketProvider>
+        <App />
+      </SocketProvider>
     </Provider>
   </React.StrictMode>,
 );
